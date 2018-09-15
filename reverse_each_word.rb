9a01ff538d93029e68f2ab570f2363329a13bb3e
Stef Claus <stef.claus@gmail.com>
2018-09-15 21:24:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,16 @@
 
 
 def reverse_each_word(sentence)
+<<<<<<< HEAD
   new_array = sentence.split(" ")
   new_array.collect {|x| x.reverse!}
   new_array.join(" ")
 end 
+=======
+  sentence.split
+  sentence.collect do |word|
+    word.reverse!
+  end #ends iteration
+    sentence.join
+end #ends method
+>>>>>>> 97f359701afdf0483ac2caa113d6bfcba009004e
